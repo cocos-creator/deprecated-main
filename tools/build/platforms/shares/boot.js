5bc3bf56f61b1675075ea2503e3eafd360df7611
Jare Guo <jare85@163.com>
2015-02-28 16:10:01 +0800

    update boot.js

--- tools/build/platforms/shares/boot.js
+++ tools/build/platforms/shares/boot.js
@@ -16,7 +16,7 @@
     Fire.AssetLibrary.init('resource/library');
     // load scene
     loadProjectSettings(function (project) {
-        Fire.Engine.loadScene(project.scenes[0], null,
+        Fire.Engine._loadSceneByUuid(project.scenes[0], null,
             function () {
                 Fire.Engine.play();
             }
