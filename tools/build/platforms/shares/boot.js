f92da9ce75fe10319aeb156e9d1cdc97470b0c6a
Jare Guo <jare85@163.com>
2015-03-20 18:03:29 +0800

    fireball-x/dev#424: adjust builded html

--- tools/build/platforms/shares/boot.js
+++ tools/build/platforms/shares/boot.js
@@ -1,4 +1,5 @@
 ﻿;window.onload = function () {
+
     function loadProjectSettings (callback) {
         Fire._JsonLoader('settings.json', function (error, json) {
             if (error) {
@@ -13,8 +14,13 @@
         // init engine
         var canvas = document.getElementById('GameCanvas');
         Fire.Engine.init(canvas.width, canvas.height, canvas, settings);
+
+        // makes the container's size equals to the frame's size
+        Fire.Screen.ContainerStrategy.EqualToFrame.apply();
+
         // init assets
         Fire.AssetLibrary.init('resource/library');
+
         // load scene
         var launchUuid = settings.scenes[settings.launchScene];
         Fire.Engine._loadSceneByUuid(launchUuid, null,
