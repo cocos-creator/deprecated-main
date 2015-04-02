49aff5d93f3dcc2f9ae79e9cc0cc53bd617efa32
Nan Wang <nantas@gmail.com>
2015-04-02 15:30:27 +0800

    use segment.io instead of mixpanel to prevent GFW blocking request.

--- metrics.js
+++ metrics.js
@@ -3,47 +3,46 @@
  * Created by nantas on 15/3/25.
  */
 var startTime;
-
 var Fire = Fire || {};
 Fire.Metrics = {
     //mixpanel event
     trackDashboardOpen: function() {
-        if (mixpanel)
-            mixpanel.track("Dashboard Open");
+        if (analytics)
+            analytics.track("Dashboard Open");
     },
     trackEditorOpen: function() {
         startTime = new Date();
-        if (mixpanel)
-            mixpanel.track("Editor Open");
+        if (analytics)
+            analytics.track("Editor Open");
     },
     trackEditorClose: function() {
         var duration = (new Date() - startTime)*1000/60; //calculate minutes
-        if (mixpanel)
-            mixpanel.track("Editor Close", {
+        if (analytics)
+            analytics.track("Editor Close", {
                 "Duration In Minutes": duration
             });
     },
     trackCreateNewScene: function() {
-        if (mixpanel)
-            mixpanel.track("Create New Scene");
+        if (analytics)
+            analytics.track("Create New Scene");
     },
     trackOpenScene: function() {
-        if (mixpanel)
-            mixpanel.track("Open Scene");
+        if (analytics)
+            analytics.track("Open Scene");
     },
     trackPlayInEditor: function() {
-        if (mixpanel)
-            mixpanel.track("Play In Editor");
+        if (analytics)
+            analytics.track("Play In Editor");
     },
     trackBuild: function(target) {
-        if (mixpanel)
-            mixpanel.track("Build", {
+        if (analytics)
+            analytics.track("Build", {
                 "Target Platform": target
             });
     },
     trackOpenCodeEditor: function() {
-        if (mixpanel)
-            mixpanel.track("Open Code Editor");
+        if (analytics)
+            analytics.track("Open Code Editor");
     }
     //trackEditorCrash:
 };
