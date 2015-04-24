4562e07761fb13f79e6609a0d78d618283be2dc7
Johnny Wu <KarasAya@gmail.com>
2015-04-24 20:04:38 +0800

    update fireball-x/dev#564

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // init your app
 var Fs = require('fire-fs');
 var Path = require('fire-path');
+var Ipc = require('ipc');
+var Crypto = require('crypto');
+var Getmac = require('getmac');
 
 function _initFire () {
     // load and mixin Fire module
@@ -20,6 +23,106 @@ function _initFire () {
     // mixin Fire.Editor to Editor
     Fire.JS.mixin( Editor,  Fire.Editor);
 }
+
+function _initLoginIpc () {
+    Ipc.on('login:query-info', function ( reply ) {
+        var fireballProfile = Editor.loadProfile( 'fireball', 'global' );
+        var lastLoginAccount = fireballProfile['last-login'];
+        var rememberPasswd = fireballProfile['remember-passwd'];
+
+        // get password
+        if ( rememberPasswd ) {
+            Getmac.getMac( function ( err, macAddress) {
+                if ( err ) {
+                    reply({
+                        'account': lastLoginAccount,
+                        'remember-passwd': rememberPasswd,
+                        'password': '',
+                    });
+                    return;
+                }
+
+                var infoFile = Path.join(Editor.dataPath,'.info');
+                if ( Fs.existsSync(infoFile) ) {
+                    var info = JSON.parse(Fs.readFileSync(infoFile));
+                    var crypted = info[lastLoginAccount];
+                    var passwd = '';
+                    if ( crypted ) {
+                        var decipher = Crypto.createDecipher('aes-256-cbc', macAddress);
+                        decipher.update( crypted, 'hex', 'utf8' );
+                        passwd = decipher.final('utf8');
+                    }
+
+                    reply({
+                        'account': lastLoginAccount,
+                        'remember-passwd': rememberPasswd,
+                        'password': passwd,
+                    });
+                }
+            });
+        }
+        else {
+            reply({
+                'account': lastLoginAccount,
+                'remember-passwd': rememberPasswd,
+                'password': '',
+            });
+        }
+    });
+
+    Ipc.on('login:save', function ( detail ) {
+        var fireballProfile = Editor.loadProfile( 'fireball', 'global' );
+        if ( detail.account !== undefined ) {
+            fireballProfile['last-login'] = detail.account;
+        }
+        if ( detail['remember-passwd'] !== undefined ) {
+            fireballProfile['remember-passwd'] = detail['remember-passwd'];
+
+            // remove passwd if remember is false
+            if ( detail['remember-passwd'] === false ) {
+                var infoFile = Path.join(Editor.dataPath,'.info');
+                if ( Fs.existsSync(infoFile) ) {
+                    var info = JSON.parse(Fs.readFileSync(infoFile));
+                    if ( info[detail.account] ) {
+                        delete info[detail.account];
+                    }
+                    Fs.writeFileSync(infoFile, JSON.stringify(info, null, 2));
+                }
+            }
+        }
+        fireballProfile.save();
+    });
+
+    Ipc.on('login:succeed', function ( detail ) {
+        var fireballProfile = Editor.loadProfile( 'fireball', 'global' );
+        fireballProfile['last-login'] = detail.account;
+        fireballProfile['remember-passwd'] = detail['remember-passwd'];
+        fireballProfile.save();
+
+        if ( detail['remember-passwd'] ) {
+            Getmac.getMac( function ( err, macAddress) {
+                if ( err ) {
+                    Editor.error( 'Failed to remember your passwd.');
+                    return;
+                }
+
+                var chipher = Crypto.createCipher('aes-256-cbc', macAddress);
+                chipher.update(detail.password,'utf8','hex');
+                var crypted = chipher.final('hex');
+
+                var infoFile = Path.join(Editor.dataPath,'.info');
+                var info = {};
+
+                if ( Fs.existsSync(infoFile) ) {
+                    info = JSON.parse(Fs.readFileSync(infoFile));
+                }
+                info[detail.account] = crypted;
+                Fs.writeFileSync(infoFile, JSON.stringify(info, null, 2));
+            });
+        }
+    });
+}
+
 var _projectPath = null;
 var _requireLogin = false;
 
@@ -36,12 +139,15 @@ module.exports = {
     init: function ( options ) {
         Editor.log( 'Initializing fire' );
         _initFire();
+        _initLoginIpc();
 
         Fire.info('Welcome to Fireball! The next-gen html5 game engine.');
 
         // load ~/.fireball/fireball.json
         Editor.loadProfile( 'fireball', 'global', {
-            recentlyOpened: [],
+            'recently-opened': [],
+            'last-login': '',
+            'remember-passwd': true,
         });
 
         if ( options.args.length > 0 ) {
