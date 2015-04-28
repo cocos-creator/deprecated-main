fdeda148f8814370655c2280da314eff36be24cf
Johnny Wu <KarasAya@gmail.com>
2015-04-28 23:06:18 +0800

    simplify Editor.Panel.load

--- panel-ready.js
+++ panel-ready.js
@@ -1,45 +1,31 @@
-// NOTE: there are two way to initialize a panel,
-// panel:ready happends when a panel open in a new window
-// panel:open happends when a panel open in a exists window
-
 if ( Editor.argv.panelID ) {
-    Editor.sendRequestToCore( 'panel:query-info', Editor.argv.panelID,
-                            function ( detail ) {
-        var panelInfo = detail['panel-info'];
-        var packagePath = detail['package-path'];
+    Editor.Panel.load( Editor.argv.panelID, function ( err, viewEL, panelInfo ) {
+        if ( panelInfo.type === 'dockable' ) {
+            var dock = new FireDock();
+            dock.setAttribute('fit', '');
+            dock.setAttribute('no-collapse', '');
 
-        var Path = require('fire-path');
-        Editor.Panel.load( Path.join( packagePath, panelInfo.view ),
-                            Editor.argv.panelID,
-                            panelInfo,
-                            function ( err, element ) {
-                                if ( panelInfo.type === 'dockable' ) {
-                                    var dock = new FireDock();
-                                    dock.setAttribute('fit', '');
-                                    dock.setAttribute('no-collapse', '');
+            var panel = new FirePanel();
+            panel.add(viewEL);
+            dock.appendChild(panel);
+            document.body.appendChild(dock);
 
-                                    var panel = new FirePanel();
-                                    panel.add(element);
-                                    dock.appendChild(panel);
-                                    document.body.appendChild(dock);
+            EditorUI.DockUtils.root = dock;
+        }
+        else {
+            document.body.appendChild(viewEL);
 
-                                    EditorUI.DockUtils.root = dock;
-                                }
-                                else {
-                                    document.body.appendChild(element);
+            EditorUI.DockUtils.root = viewEL;
+        }
+        EditorUI.DockUtils.reset();
 
-                                    EditorUI.DockUtils.root = element;
-                                }
-                                EditorUI.DockUtils.reset();
+        Editor.sendToCore('panel:ready', Editor.argv.panelID);
 
-                                Editor.sendToCore('panel:ready', Editor.argv.panelID);
-
-                                // save layout after css layouted
-                                window.requestAnimationFrame ( function () {
-                                    Editor.sendToCore( 'window:save-layout',
-                                                    Editor.Panel.getLayout(),
-                                                    Editor.requireIpcEvent );
-                                });
-                            } );
-    } );
+        // save layout after css layouted
+        window.requestAnimationFrame ( function () {
+            Editor.sendToCore( 'window:save-layout',
+                              Editor.Panel.getLayout(),
+                              Editor.requireIpcEvent );
+        });
+    });
 }
