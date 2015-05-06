81fd41687db5633ed21c187d501c0d9ac1e913c7
Nan Wang <nantas@gmail.com>
2015-05-06 12:59:11 +0800

    fireball-x/dev#648 override document set cookie method to use localstorage to store cookie, this makes mixpanel able to track recurring users.

--- metrics.js
+++ metrics.js
@@ -4,6 +4,52 @@
  */
 var startTime;
 var Editor = Editor || {};
+
+(function(document) {
+    localStorage.cookies || (localStorage.cookies = '{}');
+    document.__defineGetter__('cookie', function() {
+      var cookieName, cookies, output, val;
+      cookies = JSON.parse(localStorage.cookies);
+      output = [];
+      for (cookieName in cookies) {
+        val = cookies[cookieName];
+        output.push(cookieName + '=' + val);
+      }
+      return output.join(';');
+    });
+    document.__defineSetter__('cookie', function(s) {
+      var cookies, indexOfSeparator, key, value;
+      indexOfSeparator = s.indexOf('=');
+      key = s.substr(0, indexOfSeparator);
+      value = s.substring(indexOfSeparator + 1);
+      cookies = JSON.parse(localStorage.cookies);
+      cookies[key] = value;
+      localStorage.cookies = JSON.stringify(cookies);
+      return key + '=' + value;
+    });
+    document.clearCookies = function() {
+      delete localStorage.cookies;
+    };
+    document.__defineGetter__('location', function() {
+      var url;
+      url = 'fireball-x.com';
+      return {
+        href: 'http://' + url,
+        protocol: 'http:',
+        host: url,
+        hostname: url,
+        port: '',
+        pathname: '/',
+        search: '',
+        hash: '',
+        username: '',
+        password: '',
+        origin: 'http://' + url
+      };
+    });
+    return document.__defineSetter__('location', function() {});
+  })(document);
+  
 Editor.Metrics = {
     //segment identification
     identifyUser: function(user) {//userId, email, username, fullname, company, newsletter
