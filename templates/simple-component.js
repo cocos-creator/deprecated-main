bb0f5f158b25745240667d606a5df375de344dc8
Johnny Wu <KarasAya@gmail.com>
2015-05-13 17:15:27 +0800

    update template

--- templates/simple-component.js
+++ templates/simple-component.js
@@ -1,23 +1,25 @@
 var Comp = Fire.Class({
     extends: Fire.Component,
-    
+
     properties: {
         // foo: {
         //     default: null,
         //     type: Fire.Entity,
-        //     serializable: false,
-        //     visible: false
+        //     serializable: true, // [optional], default is true
+        //     visible: true,      // [optional], default is true
+        //     displayName: 'Foo', // [optional], default is property name
+        //     readonly: false,    // [optional], default is false
         // },
         // ...
     },
-    
+
     // use this for initialization
     start: function () {
-        
+
     },
 
     // called every frame
     update: function () {
-        
+
     }
 });
